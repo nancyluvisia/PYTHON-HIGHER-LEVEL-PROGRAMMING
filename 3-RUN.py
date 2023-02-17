974a9a7850caf762327576fc1d2c11a2757eacdf
nancyluvisia <nancieluvisia@gmail.com>
2023-02-17 04:22:33 -0800

    run

---
+++ 3-RUN.py
@@ -0,0 +1,10 @@
+#bodmas
+import math
+b=4
+a=2
+c=1
+w=(b**2-b*a*c)
+y=math.sqrt(w)
+print(y)
+x=(-b-y)/2*a
+print(f" x is equal to {x}")
---
+++ 4-RUN.py
@@ -0,0 +1,8 @@
+print("positive,negative,zero")
+num=int(input("Enter number "))
+if num>0:
+    print("number is positive")
+elif num<0:
+    print("number is negative")
+else:
+    print("it is zero")
